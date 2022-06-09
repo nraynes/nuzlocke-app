dea4a1c631216d3069689a9d27a3b531294a463e
Nicholas Raynes <nicholas.r.raynes@gmail.com>
2022-06-09 14:27:19 -0600

    finished crud functionality

--- src/NuzlockeTracker.jsx
+++ src/NuzlockeTracker.jsx
@@ -15,16 +15,22 @@ import {
 import { Box } from '@mui/system';
 import DeleteIcon from '@mui/icons-material/Delete';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import EditIcon from '@mui/icons-material/Edit';
+import CancelIcon from '@mui/icons-material/Cancel';
+import DoneIcon from '@mui/icons-material/Done';
 import compareArrays from './utils/compareArrays';
 import { setCookie, getCookie, clearCookie } from './utils/cookies';
 import addTime from './utils/addTime';
 
 function NuzlockeTracker(props) {
   const [data, setData] = useState([]);
+  const [editing, setEditing] = useState(null);
   const routeRef = useRef();
   const pokemonRef = useRef();
   const routeSearchRef = useRef();
   const pokemonSearchRef = useRef();
+  const routeEditRef = useRef();
+  const pokemonEditRef = useRef();
 
   const cookieName = 'tableData';
   const cookieData = getCookie(cookieName);
@@ -53,6 +59,7 @@ function NuzlockeTracker(props) {
     const newData = [...data];
     newData.push({
       index: data[data.length-1] ? data[data.length-1].index + 1 : 0,
+      dead: false,
       route,
       pokemon,
     })
@@ -98,6 +105,25 @@ function NuzlockeTracker(props) {
       setData(newData);
     }
   };
+
+  const editRow = (itemIndex) => {
+    const newRoute = routeEditRef.current.value;
+    const newPokemon = pokemonEditRef.current.value;
+    const newData = data.map((item) => {
+      if (item.index === itemIndex) {
+        return {
+          index: item.index,
+          dead: item.dead,
+          route: newRoute,
+          pokemon: newPokemon,
+        }
+      }
+      return item;
+    })
+    setCookie(cookieName, newData, expiration);
+    setData(newData);
+    setEditing(null)
+  }
 
   return (
     <Box
@@ -151,17 +177,23 @@ function NuzlockeTracker(props) {
                     key={`cell_${Math.floor(Math.random() * 100)}_${i}_1`}
                     sx={{
                       borderRight: '1px solid gray',
-                      maxWidth: '15em',
+                      width: '6em',
                       overFlow: 'scroll',
                     }}
                   >
-                    <Typography
-                      sx={{
-                        overflowWrap: 'break-word',
-                      }}
-                    >
-                      {item.route}
-                    </Typography>
+                    {editing === item.index
+                      ? (
+                        <TextField variant="standard" inputRef={routeEditRef} defaultValue={item.route} />
+                      )
+                      : (
+                        <Typography
+                          sx={{
+                            overflowWrap: 'break-word',
+                          }}
+                        >
+                          {item.route}
+                        </Typography>
+                      )}
                   </TableCell>
                   <TableCell
                     key={`cell_${Math.floor(Math.random() * 100)}_${i}_2`}
@@ -176,24 +208,73 @@ function NuzlockeTracker(props) {
                           maxWidth: '75%',
                         }}
                       >
-                        <Typography
-                          sx={{
-                            overflowWrap: 'break-word',
-                          }}
-                        >
-                          {item.pokemon}
-                        </Typography>
+                        {editing === item.index
+                          ? (
+                            <TextField variant="standard" inputRef={pokemonEditRef} defaultValue={item.pokemon} />
+                          )
+                          : (
+                            <Typography
+                              sx={{
+                                overflowWrap: 'break-word',
+                              }}
+                            >
+                              {item.pokemon}
+                            </Typography>
+                          )}
                       </Box>
-                      <Tooltip title="Delete row">
-                        <IconButton
-                          onClick={() => deleteRow(item.index)}
-                          sx={{
-                            width: 'max-content',
-                          }}
-                        >
-                          <DeleteIcon />
-                        </IconButton>
-                      </Tooltip>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                        }}
+                      >
+                        {editing === item.index
+                          ? (
+                            <>
+                              <Tooltip title="Submit row">
+                                <IconButton
+                                  onClick={() => editRow(item.index)}
+                                  sx={{
+                                    width: 'max-content',
+                                  }}
+                                >
+                                  <DoneIcon />
+                                </IconButton>
+                              </Tooltip>
+                              <Tooltip title="Cancel editing">
+                                <IconButton
+                                  onClick={() => setEditing(null)}
+                                  sx={{
+                                    width: 'max-content',
+                                  }}
+                                >
+                                  <CancelIcon />
+                                </IconButton>
+                              </Tooltip>
+                            </>
+                          )
+                          : (
+                            <Tooltip title="Edit row">
+                              <IconButton
+                                onClick={() => setEditing(item.index)}
+                                sx={{
+                                  width: 'max-content',
+                                }}
+                              >
+                                <EditIcon />
+                              </IconButton>
+                            </Tooltip>
+                          )}
+                        <Tooltip title="Delete row">
+                          <IconButton
+                            onClick={() => deleteRow(item.index)}
+                            sx={{
+                              width: 'max-content',
+                            }}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        </Tooltip>
+                      </Box>
                     </Box>
                   </TableCell>
                 </TableRow>
